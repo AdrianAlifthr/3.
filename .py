fbaa7ce0b8f4f80813a859d9e52df8baa8e9f55d
Adrianalifthr <adrianalifathir77@gmail.com>
2024-11-28 18:42:46 +0700

    PTI CHATBOT

--- .py
+++ .py
@@ -2,8 +2,8 @@ import os
 from flask import Flask, render_template, request, jsonify
 import google.generativeai as genai
 
-# Periksa konfigurasi API key dengan cara yang lebih aman
-genai.configure(api_key= "AIzaSyAv5YmcpdWtw-9CE5B1SnlalMJ0igHV8Wg")  # Menyimpan key di environment variabel
+
+genai.configure(api_key="AIzaSyAv5YmcpdWtw-9CE5B1SnlalMJ0igHV8Wg")
 
 generation_config = {
     "temperature": 1,
@@ -13,37 +13,41 @@ generation_config = {
     "response_mime_type": "text/plain",
 }
 
-# Anda bisa mengganti model_name jika menggunakan model yang lebih spesifik atau sesuai
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash",  # Periksa nama model yang sesuai dengan dokumentasi terbaru
+    model_name="gemini-1.5-flash",  
     generation_config=generation_config,
 )
 
+
 app = Flask(__name__)
 
+
 def GeneratedResponse(input_text):
-    # Menyusun prompt dengan cara yang benar
+    
     response = model.generate_content([
-        "Kamu adalah seseorang yang bernama BOTO, kamu merupakan pemandu pariwisata di Sumatera Utara, "
-        "kamu mengetahui tempat wisata dan tempat penginapan di Sumatera Utara serta beri alamat tempatnya "
-        "dan link Google Map tempat wisata dan penginapan. Kamu juga dapat menjawab pertanyaan yang bersangkutan "
-        "dengan pernyataanmu sebelumnya. Kamu tahu cafe yang ada di Sumatera Utara. ",
+        "Kamu adalah seseorang yang bernama BOTO, kamu merupakan pemandu pariwisata di Sumatera Utara. "
+        "Jawablah dalam bentuk poin-poin. Untuk setiap tempat wisata atau penginapan, beri nama, alamat, "
+        "dan link Google Map-nya.",
         f"User  : {input_text}",
         "Si Boto   : ",
     ])
     
-    return response.text
+    response_text = response.text.strip()
+    response_list = response_text.split('\n') 
+    return response_list
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/get_response', methods=['POST'])
 def get_response():
     user_input = request.form['user_input']
-    bot_response = GeneratedResponse(user_input)
-    return jsonify({'response': bot_response})
+    bot_response_list = GeneratedResponse(user_input) 
+    return jsonify({'response': bot_response_list})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
- 
